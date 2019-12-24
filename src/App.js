edf31980eda16f4583803ae957269e64850b0fd3
Artem Osmerkin <artem.osmerkin@firstwavecloud.com>
2019-12-24 14:27:25 +1100

    Changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there again, again, again! Cool! Cool!
+          Hi there!
         </p>
         <a
           className="App-link"
